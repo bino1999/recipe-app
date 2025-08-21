2e63d8cc68a96077ecb322615ae775cc3e39bd72
binodbandara <binod.bandara@ivedha.com>
2025-08-21 18:54:45 +0530

    commit

--- server/server.mjs
+++ server/server.mjs
@@ -8,7 +8,7 @@ const server = express();
 server.use(express.json());
 const port = process.env.PORT || 4001;
 const corsOptions ={
-    origin:['http://localhost:5173','https://recipe-app-five-murex-86.vercel.app/' ],
+    origin:['http://localhost:5173','https://recipe-app-five-murex-86.vercel.app' ],
     credentials:true,            
     optionSuccessStatus:200
 }
